11b24bab46112df95c4fbecac1f044394811be8c
Risav pyakurel <77790895+risav-pyakurel@users.noreply.github.com>
2023-04-30 23:46:34 +0545

    Add files via upload

--- py/controlflows.py
+++ py/controlflows.py
@@ -1,7 +1,12 @@
-
+"""""
 # if conditions
 
 val= input("enter the number")
 value_float= float(val)
 if(value_float>100):
-    print("the number is greater than 100")
+    'print("the number is greater than 100")
+    """
+val= input("enter the number")
+value_float= float(val)
+if(value_float%2 ==00):
+    print("the number is even")
