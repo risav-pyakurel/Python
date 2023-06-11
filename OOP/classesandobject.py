64ffaa2d84bc3200b76cd75a2737c9c16b898688
risav pyakurel <pyakurel.risav@gmail.com>
2023-06-11 17:14:22 +0545

    my second contribution

---
+++ OOP/classesandobject.py
@@ -0,0 +1,41 @@
+
+'''class Car:
+    pass
+ford = Car()
+honda= Car()
+audi= Car()
+
+ford.speed= 200
+honda.speed= 430
+audi.speed= 678
+
+ford.color= 'red'
+honda.color= 'black'
+audi.color= 'grey'
+
+
+print(audi.speed)
+print(ford.color)
+
+
+
+
+audi.speed= '6799'
+
+print(audi.speed)'''
+
+class Rectangle:
+    pass
+rect1= Rectangle()
+rect2= Rectangle()
+
+rect1.height= 89
+rect2.height= 56
+
+rect1.width= 5
+rect2.width= 78
+
+
+print(rect1.height*rect1.width)
+
+print(rect2.height*rect2.width)
