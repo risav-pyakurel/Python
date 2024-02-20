f1305600e595d5538c56bedb146e6e60cd88c09b
Risav pyakurel <77790895+risav-pyakurel@users.noreply.github.com>
2024-02-20 23:28:03 +0545

    Add files via upload

--- functions/solutions8.py
+++ functions/solutions8.py
@@ -1,8 +1,11 @@
 # create a function that accepts any number of keyword arguments and prints them in the format key : Value
 
-def print_kwargs(name,power):
-  print("Name ",name, "Power:" , power)
-
+def print_kwargs(**kwargs):
+  for key, value in kwargs.items():
+    print(f"{key}:{value}")
+ 
 
 
 print_kwargs(name= "Risav", power = "bigtime momo eater")
+print_kwargs(name= "Risav")
+print_kwargs(name= "Risav", power = "bigtime momo eater", studies ="CS")
