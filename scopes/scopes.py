1164e66553c0b72bb8240b7d836a1469bb990791
Risav pyakurel <77790895+risav-pyakurel@users.noreply.github.com>
2024-02-21 22:54:23 +0545

    Add files via upload

---
+++ scopes/scopes.py
@@ -0,0 +1,18 @@
+# username = "momoandsarcasm"
+
+# def func():
+#   username = "momo"
+#   print(username)
+
+# print(username)
+# func()
+
+x = 99
+def func2(y):
+  z = x+y
+  return z
+
+result = func2(1)
+
+print(result)
+
