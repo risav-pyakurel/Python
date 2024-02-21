9dc2ebde01105718d44bc03515295ee44c080ecb
Risav pyakurel <77790895+risav-pyakurel@users.noreply.github.com>
2024-02-21 22:13:14 +0545

    Add files via upload

---
+++ functions/solutions9.py
@@ -0,0 +1,13 @@
+# create a  generator function that yields even number upto a specified limit
+
+ 
+
+def even_generator(limit):
+  
+
+  for i in range(2, limit +1, 2):
+   yield i
+
+
+for num in even_generator(10):
+  print(num)
