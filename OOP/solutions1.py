19405810eb2d846a05c42092bd3a0e007df35d4d
Risav pyakurel <77790895+risav-pyakurel@users.noreply.github.com>
2024-02-27 15:10:50 +0545

    Add files via upload

---
+++ OOP/solutions1.py
@@ -0,0 +1,15 @@
+# create a car class with attributes like brand and model. then create an instance of this class 
+
+class Car:
+  def __init__(self, brand,model):
+    self.brand = brand
+    self.model = model
+
+
+
+my_car= Car("Toyota", "Corolla")
+print(my_car.brand)
+print(my_car.model)
+
+my_new_car = Car("Tata","Safari")
+print(my_new_car.model)
---
+++ OOP/solutions3.py
@@ -0,0 +1,30 @@
+# create an electric carf class that inherits from the car class and has an additional attribute battery_size.
+
+
+class Car:
+  def __init__(self, brand,model): #constructor __init__
+    self.brand = brand
+    self.model = model
+
+  def full_name(self):
+    return f"{self.brand} {self.model}"
+    
+
+class Electriccar(Car):
+  def __init__(self,brand, model , battery_size):
+    super().__init__(brand, model)
+    self.battery_size = battery_size
+
+my_tesla = Electriccar("BYD", "Model Dolphin","85 KwH")
+print(my_tesla.model)
+print(my_tesla.full_name())
+
+
+# my_car= Car("Toyota", "Corolla")
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.full_name())
+
+# my_new_car = Car("Tata","Safari")
+# print(my_new_car.model)
+
