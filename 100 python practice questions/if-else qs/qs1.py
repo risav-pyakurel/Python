a52de12d25718ff4d9fa498d5c16a4994db8ced6
risav pyakurel <pyakurel.risav@gmail.com>
2024-04-26 21:55:16 +0545

    new

---
+++ 100 python practice questions/if-else qs/qs1.py
@@ -0,0 +1,9 @@
+# accept two numbers and print the greatest between them 
+
+a= int(input("Enter 1st number :"))
+b= int(input("Enter 2nd number:"))
+
+if (a>b):
+  print ("1st number is greatest")
+else:
+  print("2nd number is greatest")
---
+++ 100 python practice questions/if-else qs/qs2.py
@@ -0,0 +1,9 @@
+# accept the gender from the user as char and print the respective greeting msg
+
+gender = chr(input("Enter your gender :"))
+
+if (gender == male):
+  print ("Good Morning Sir")
+else:
+    print ("Good Morning madam")
+ 
