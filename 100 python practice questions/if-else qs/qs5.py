b9eed38e388ee149bfc0bf94f90dbf22260f0045
risav pyakurel <pyakurel.risav@gmail.com>
2024-04-29 21:07:27 +0545

    another one

--- 100 python practice questions/if-else qs/qs5.py
+++ 100 python practice questions/if-else qs/qs5.py
@@ -2,7 +2,10 @@
 
 year = int(input("Please type the year: "))
 
-if year%4 ==0:
+if year%4 ==0 and year %100 != 0:
+
   print(f"{year} is a leap year")
+elif year %100 ==0 and year %400 == 0:
+  print (f"{year} is a leap year ")
 else:
   print(f"{year} isn't a leap year")
---
+++ 100 python practice questions/if-else qs/qs6.py
@@ -0,0 +1,2 @@
+# accept an english alphabet from user and check if it is a consonant or a vowel
+
