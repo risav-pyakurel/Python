3cbf8e43caec5d6301408d1656c7a9ff4e9df4fb
risav-pyakurel <pyakurel.risav@gmail.com>
2024-08-02 12:12:19 +0545

    logic building

---
+++ aug1/functions.py
@@ -0,0 +1,75 @@
+# def cal_sum(a, b):
+#     sum = a+b
+#     print(sum)
+#
+#
+# cal_sum(1, 2)
+# cal_sum(1, 3)
+#
+
+
+#def cal_sum(a, b):  #parameters
+#return a + b
+
+
+# print(cal_sum(22, 33))
+# print(cal_sum(44, 66))
+
+#sum = cal_sum(55, 66)  #function call; a,b = arguments
+#print(sum)
+
+#
+# def print_hello():
+#     print("Hello")
+#
+#
+# print_hello()
+
+
+# def avg_numbers(a, b, c):
+#     avg = (a + b + c) / 3
+#     print(f"Average of numbers : {avg}")
+#
+#
+# avg_numbers(33, 55, 78)
+#
+#
+# def cal_prod(a, b):
+#     return a * b
+#
+# cal_prod()
+
+
+# cities = ['chitwan', 'kathmandu', 'lalitpur', 'bhaktapur']
+# attract = ['sauraha', 'basantapur', 'patan', 'bhedasingh']
+#
+#
+# def print_len(list):
+#     print(len(list))
+#
+#
+# def print_list(list):
+#     for item in list:
+#         print(item, end=" ")
+#
+# print_list(attract)
+# print()
+
+# n = int(input("enter the number: "))
+
+
+# def cal_fact(n):
+#     fact = 1
+#     for i in range(1, n + 1):
+#         fact = fact * i
+#     print(fact)
+#
+#
+# cal_fact(n)
+
+# def converter(usd_val):
+#     npr_val = usd_val * 135
+#     print(usd_val, "USD=", npr_val, "NPR")
+#
+#
+# converter(2800)
